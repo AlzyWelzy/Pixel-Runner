f715cd68616c4b5dc34013ce3e8702219469ddc8
AlzyWelzy <welzyalzy@gmail.com>
2023-02-14 21:56:08 +0530

    .

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import pygame  # importing pygame
 pygame.init()
 # screen = pygame.display.set_mode((width, height))
 screen = pygame.display.set_mode((800, 400))
+
+while True:
+    # draw all our elements
+    # update everythin
+    pygame.display.update()
