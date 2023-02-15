b0505fe75b91485bf1ce96f3b2dd37a430987f9a
AlzyWelzy <welzyalzy@gmail.com>
2023-02-15 13:38:08 +0530

    .

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame  # importing pygame
 
 pygame.init()
-screen = pygame.display.set_mode((width, height))
+# screen = pygame.display.set_mode((width, height))
 screen = pygame.display.set_mode((800, 400))
 
 while True:
