ab09b107bac59c6fa67ff6771b6df988e5c19a18
AlzyWelzy <welzyalzy@gmail.com>
2023-02-19 11:35:52 +0530

    added ground

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill("Red")
+sky_surface = pygame.image.load("./graphics/Sky.png")
+ground_surface = pygame.image.load("./graphics/ground.png")
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -17,7 +18,8 @@ while True:
     # draw all our elements
     # update everything
 
-    screen.blit(test_surface, (200, 100))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
 
     pygame.display.update()
     clock.tick(60)
