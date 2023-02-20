e2aa70b293e4b84408d37b28331bfaaf72fdce82
AlzyWelzy <welzyalzy@gmail.com>
2023-02-20 12:34:23 +0530

    added the game restart functionality

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("./font/Pixeltype.ttf", 50)
+game_active = True
+
 
 sky_surface = pygame.image.load("./graphics/sky.png").convert()
 ground_surface = pygame.image.load("./graphics/ground.png").convert()
@@ -32,13 +34,19 @@ while True:
             pygame.quit()
             exit()
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
-                player_gravity = -20
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                    player_gravity = -20
+                    game_active = True
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-                player_gravity = -20
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
 
         # if event.type == pygame.KEYUP:
         #     print("Key Up")
@@ -59,42 +67,46 @@ while True:
         # if event.type == pygame.KEYDOWN:
         #     if event.key == pygame.K_SPACE:
         #         player_gravity = -20
+    if game_active:
+        screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, 300))
+        # screen.blit(text_surface, (300, 50))
+        pygame.draw.rect(screen, "#c0e8ec", score_rect)
+        pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+        screen.blit(score_surf, score_rect)
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    # screen.blit(text_surface, (300, 50))
-    pygame.draw.rect(screen, "#c0e8ec", score_rect)
-    pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
-    screen.blit(score_surf, score_rect)
+        # pygame.draw.line(screen, "Red", (0, 0), (800, 400), 1)
+        # pygame.draw.line(screen, "Red", (0, 0), pygame.mouse.get_pos(), 1)
+        # pygame.draw.ellipse(screen, "Grey", pygame.Rect(50, 200, 100, 100))
 
-    # pygame.draw.line(screen, "Red", (0, 0), (800, 400), 1)
-    # pygame.draw.line(screen, "Red", (0, 0), pygame.mouse.get_pos(), 1)
-    # pygame.draw.ellipse(screen, "Grey", pygame.Rect(50, 200, 100, 100))
+        snail_rect.x -= 4
+        if snail_rect.right <= 0:
+            snail_rect.left = 800
+        screen.blit(snail_surf, snail_rect)
 
-    snail_rect.x -= 4
-    if snail_rect.right <= 0:
-        snail_rect.left = 800
-    screen.blit(snail_surf, snail_rect)
+        # Player
+        player_gravity += 1
+        player_rect.y += player_gravity
 
-    # Player
-    player_gravity += 1
-    player_rect.y += player_gravity
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
 
-    if player_rect.bottom >= 300:
-        player_rect.bottom = 300
+        screen.blit(player_surf, player_rect)
 
-    screen.blit(player_surf, player_rect)
+        # if player_rect.colliderect(snail_rect):
+        #     print("COLLISION")
 
-    # if player_rect.colliderect(snail_rect):
-    #     print("COLLISION")
+        # if player_rect.collidepoint(mouse_pos):
+        #     print(pygame.mouse.get_pressed())
 
-    # if player_rect.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
+        # collision
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+            # pygame.quit()
+            # exit()
 
-    # collision
-    if snail_rect.colliderect(player_rect):
-        pygame.quit()
-        exit()
+    else:
+        screen.fill("Yellow")
 
     pygame.display.update()
     clock.tick(60)
